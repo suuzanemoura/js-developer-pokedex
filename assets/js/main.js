fa97f70935fe34fb123a22780a745491e0a956ab
Suzane Moura <suuzanemoura@gmail.com>
2023-08-24 20:02:42 -0300

    :lipstick: feat: Excluindo BootStrap e mudando a estilização CSS global e da pokelist

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,20 +1,31 @@
 const pokemonList = document.getElementById("pokeList");
 const loadMoreButton = document.getElementById("loadMoreButton");
 
-const maxRecords = 151;
-const limit = 10;
+const maxRecords = 1008;
+const limit = 60;
 let offset = 0;
 
 function convertPokemonToLi(pokemon) {
   return `
         <li class="pokemon ${pokemon.type}">
-            <span class="number">#${pokemon.number}</span>
+            <span class="number">#${
+              pokemon.number < 10
+                ? "00" + pokemon.number
+                : pokemon.number > 10 && pokemon.number < 100
+                ? "0" + pokemon.number
+                : pokemon.number
+            }</span>
             <span class="name">${pokemon.name}</span>
 
             <div class="detail">
                 <ol class="types">
                     ${pokemon.types
-                      .map((type) => `<li class="type ${type}">${type}</li>`)
+                      .map(
+                        (type) =>
+                          `<li class="type ${type}">${
+                            type.charAt(0).toUpperCase() + type.slice(1)
+                          }</li>`,
+                      )
                       .join("")}
                 </ol>
 
